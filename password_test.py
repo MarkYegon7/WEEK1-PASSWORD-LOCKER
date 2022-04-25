a3b6ff86cccc7970b2144e833c3a41acdfbba0ab
MarkYegon7 <mark.yegon@student.moringaschool.com>
2022-04-25 16:03:43 +0300

    added docstings

--- password_test.py
+++ password_test.py
@@ -1,9 +1,7 @@
-
+import unittest
 
 def test_init(self):
-        '''
-        test_init test case to test if the object is initialized properly
-        '''
+        
 
         self.assertEqual(self.user_account.first_name,"go.com")
         self.assertEqual(self.user_account.last_name,"12345")
